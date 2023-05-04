41153629567c3fb9dcd65c4c3f9def74d562b27b
erick <erickgoncalves5@hotmail.com>
2023-05-04 14:43:54 -0300

    #1 Criar classe Pessoa

---
+++ src/main/java/org/example/Pessoa.java
@@ -0,0 +1,41 @@
+package org.example;
+
+import java.time.LocalDate;
+
+public class Pessoa {
+
+    private String nome;
+    private LocalDate dataDeNascimento;
+
+    public Pessoa(String nome, LocalDate dataDeNascimento) {
+        this.nome = nome;
+        this.dataDeNascimento = dataDeNascimento;
+    }
+
+    public Pessoa() {
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public LocalDate getDataDeNascimento() {
+        return dataDeNascimento;
+    }
+
+    public void setDataDeNascimento(LocalDate dataDeNascimento) {
+        this.dataDeNascimento = dataDeNascimento;
+    }
+
+    @Override
+    public String toString() {
+        return "Pessoa{" +
+                "nome='" + nome + '\'' +
+                ", dataDeNascimento=" + dataDeNascimento +
+                '}';
+    }
+}
